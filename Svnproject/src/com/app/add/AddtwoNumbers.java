3a0d13561a16f6e3495375d74d5fea4581962b9a
srinu <Srinu123@gmail.com>
2018-09-02 19:20:57 +0530

    AddTeonumbers

---
+++ Svnproject/src/com/app/add/AddtwoNumbers.java
@@ -0,0 +1,24 @@
+package com.app.add;
+
+public class AddtwoNumbers {
+	int a;
+	int b;
+	
+	 public AddtwoNumbers(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int Add(int a, int b){
+		 
+		 
+		 return a+b;///////////
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AddtwoNumbers add=new AddtwoNumbers(10,20);
+System.out.println(add);
+	}
+
+}
